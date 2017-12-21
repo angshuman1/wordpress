ccfedd58a43328024a0985ab7efd6f9c9ea966a3
Angshuman <asengupta.sn@gmail.com>
2017-12-21 17:15:20 +0000

    message

--- manifests/dbconfig.pp
+++ manifests/dbconfig.pp
@@ -6,12 +6,13 @@ exec { "setuprootpassword" :
                        require => Service['mysql'],
                        before => Exec['runmysqlcommand'],
 }
-exec { "downloadmysqlcommand" :
-        command => "wget https://raw.githubusercontent.com/roybhaskar9/chefwordpress-1/master/wordpress/files/default/mysqlcommands && touch /tmp/file2",
-        path => "/usr/bin",
-        cwd => "/var",
-        creates => "/tmp/file2",
-}
+#exec { "downloadmysqlcommand" :
+#        command => "wget https://raw.githubusercontent.com/roybhaskar9/chefwordpress-1/master/wordpress/files/default/mysqlcommands && touch /tmp/file2",
+#        path => "/usr/bin",
+#        cwd => "/var",
+#        creates => "/tmp/file2",
+
+file { "/etc/mysqlcommands : source => "puppet:///modules/wordpress/mysqlcommands }
 exec { "runmysqlcommand" :
                  command => "mysql -uroot -pabcd1234 < /var/mysqlcommands && touch /tmp/file3",
                  path => "/usr/bin",
--- manifests/web.pp
+++ manifests/web.pp
@@ -1,10 +1,12 @@
 class wordpress::web {
-exec { "downloadwordpress" :
-                   command => "wget https://wordpress.org/latest.zip && touch /tmp/fil4",
-                   path => "/usr/bin",
-                   cwd => "/var",
-                   creates => "/tmp/fil4",
- }
+#exec { "downloadwordpress" :
+#                   command => "wget https://wordpress.org/latest.zip && touch /tmp/fil4",
+#                   path => "/usr/bin",
+#                  cwd => "/var",
+#                   creates => "/tmp/fil4",
+
+file { "/etc/latest.zip" : source => "puppet:///modules/wordpress/latest.zip }
+
 exec { "unzipwordpress" :
             command => "unzip /var/latest.zip && touch /tmp/file5",
             path => "/usr/bin",
@@ -18,10 +20,20 @@ exec { "copywordpress" :
                   creates => "/tmp/file6",
                   require => Exec['unzipwordpress'],
 }
-exec { "configurewordpress" :
-               command => "wget https://raw.githubusercontent.com/roybhaskar9/chefwordpress-1/master/wordpress/files/default/wp-config-sample.php -O wp-config.php && touch /tmp/file7",
-               path => "/usr/bin",
-               cwd => "/var/www/html",
-               require => Exec['copywordpress'],
+
+#exec { "configurewordpress" :
+#               command => "wget https://raw.githubusercontent.com/roybhaskar9/chefwordpress-1/master/wordpress/files/default/wp-config-sample.php -O wp-config.php && touch /tmp/file7",
+#               path => "/usr/bin",
+#               cwd => "/var/www/html",
+#               require => Exec['copywordpress'],
+
+file { "/etc/wp-config.php" : source => "puppet///modules/wordpress/wp-config.php }
+
+
+file  { "/var/www/html/index.html" :
+                            ensure => absent,
+                            require => Service['apache2'],
 }
-}
+exec { "service apache2 restart" :
+                     require => Exec['configurewordpress'],
+}    
